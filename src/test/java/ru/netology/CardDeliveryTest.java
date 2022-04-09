98674d6c0985d0523fc07634c8611cc6c0506dbd
zybu <zybu@live.ru>
2022-04-09 19:47:25 +0300

    added tests

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,95 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.data.DataGenerator.*;
+
+public class CardDeliveryTest {
+
+    private final String date = generateDate(3);
+    private final String secondDate = generateDate(15);
+    private final String expiredDate = generateDate(-15);
+    private final String city = generateCity();
+    private final String name = generateName();
+    private final String phone = generatePhone();
+
+    @BeforeEach
+    void setupTest() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldOrderCardDelivery() {
+        $("[placeholder='Город']").setValue(city);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(date);
+
+        $("[name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
+        $("[class='checkbox__box']").click();
+        $(withText("Запланировать")).click();
+
+        String notificationContent;
+        notificationContent = "Встреча успешно запланирована на " + date;
+        $("[class='notification__content']").
+                shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText(notificationContent));
+    }
+
+    @Test
+    void shouldOrderNewDateCardDelivery() {
+        $("[placeholder='Город']").setValue(city);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(date);
+
+        $("[name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
+        $("[class='checkbox__box']").click();
+        $(withText("Запланировать")).click();
+
+        String notificationContent;
+        notificationContent = "Встреча успешно запланирована на " + date;
+        $("[class='notification__content']").
+                shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText(notificationContent));
+
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(secondDate);
+        $(withText("Запланировать")).click();
+
+        $$("button").find((exactText("Перепланировать"))).click();
+
+        notificationContent = "Встреча успешно запланирована на " + secondDate;
+
+        $("[class='notification__content']").
+                shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText(notificationContent));
+    }
+
+    @Test
+    void shouldGetErrorFromDate() {
+        $("[placeholder='Город']").setValue("Са");
+        $(byText("Санкт-Петербург")).click();
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(expiredDate);
+
+        $("[name='name']").setValue(name);
+        $("[name='phone']").setValue(phone);
+        $("[class='checkbox__box']").click();
+        $(withText("Запланировать")).click();
+
+        $(withText("Заказ на выбранную дату невозможен")).
+                shouldBe(visible, Duration.ofSeconds(15));
+
+    }
+}
---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+
+    public static String generateCity() {
+        List list = Arrays.asList(
+                "Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Элиста",
+                "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск",
+                "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары",
+                "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь",
+                "Владивосток", "Ставрополь", "Хабаровск", "Благовещенск", "Архангельск", "Астрахань",
+                "Белгород", "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж", "Иваново", "Орёл",
+                "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома", "Курган", "Курск",
+                "Санкт-Петербург", "Липецк", "Магадан", "Москва", "Мурманск", "Нижний Новгород",
+                "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Пенза", "Псков", "Салехард",
+                "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург",
+                "Смоленск", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск",
+                "Ярославль", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь"
+        );
+        Random item = new Random();
+        String randomElement = (String) list.get(item.nextInt(list.size()));
+        System.out.println(randomElement);
+        return randomElement;
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
+    public static String generateName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+}
